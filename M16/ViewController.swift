f35b280f8fa20189ffd1b71ba0bcc883679ad6c0
Tanya <tanyafreaky@mail.ru>
2023-04-17 19:36:34 +0300

    Init commit

---
+++ M16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M16
+//
+//  Created by Tanya on 17.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
